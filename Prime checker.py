2beb7b792d88eb590defdd2420fcd4ffbe102abe
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-21 10:41:00 +0200

    Update Prime checker.py

--- Prime checker.py
+++ Prime checker.py
@@ -4,7 +4,7 @@ number = 999983
 
 def primechecker(number):
     if number % 2 == 0:
-        print("Prime")
+        print("Not a Prime")
         Return None
     for x in range(3,int(math.sqrt(number+1)),2):
         if number % x == 0:
